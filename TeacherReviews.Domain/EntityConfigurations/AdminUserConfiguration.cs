0cb3e9afbb210f6cbc128fb3b5bf999b51026fff
Danil <wirtenberger@yandex.ru>
2023-10-13 21:08:14 +0300

    Removed HasData call in AdminUser entity configuration

--- TeacherReviews.Domain/EntityConfigurations/AdminUserConfiguration.cs
+++ TeacherReviews.Domain/EntityConfigurations/AdminUserConfiguration.cs
@@ -9,13 +9,5 @@ public class AdminUserConfiguration : IEntityTypeConfiguration<AdminUser>
     public void Configure(EntityTypeBuilder<AdminUser> builder)
     {
         builder.HasIndex(u => u.Username).IsUnique();
-        builder.HasData(
-            new AdminUser()
-            {
-                Id = 1,
-                Username = "AbsolutelyUnpredictableId",
-                Password = ""
-            }
-        );
     }
 }
